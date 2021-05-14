a4499d569c3b61c902a3ea56da12bea84559a4cd
Zlodejrs <trotsergey@list.ru>
2021-05-14 22:56:58 +0300

    mile amout

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 5000;
+        int freeMiles = 20;
+        int mileAmount = ticketPrice / freeMiles;
+        System.out.println(mileAmount);
+    }
+
+}
